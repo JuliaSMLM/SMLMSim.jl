80760a2ab0527a4e8b7227a46a2523c12c187d4c
Keith A. Lidke <kalidke@users.noreply.github.com>
2022-08-10 13:14:25 -0600

    performance updates

---
+++ examples/benchmark.jl
@@ -0,0 +1,30 @@
+using Revise
+using SMLMSim
+using Plots  
+
+## create a blinking fluorophore 
+γ=10000.0
+q=[0 10
+   1e-1 0]
+f=SMLMSim.GenericFluor(γ,q)
+
+## Simulate intensity trace 
+
+# setup sim
+nframes = 100000
+framerate = 10.0
+
+## generate CTMC
+starttime=0.0
+endtime = (nframes) / framerate
+state1=1
+ctmc=SMLMSim.CTMC(q,endtime,state1)
+
+##  generate integrated photons 
+using BenchmarkTools
+
+@btime SMLMSim.intensitytrace(f,nframes,framerate)
+
+@profview SMLMSim.intensitytrace(f,nframes,framerate)
+
+
