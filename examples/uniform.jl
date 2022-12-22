a7c5999d0cbe75224d9d83e0cfa2a1d67df6bb96
kalidke <kalidke@gmail.com>
2022-12-22 12:39:40 -0700

    create dev env, change examples to makie

--- examples/uniform.jl
+++ examples/uniform.jl
@@ -1,38 +1,51 @@
 # Generate uniform random data
 
-using Revise
 using SMLMSim
 using SMLMData
-using PlotlyJS
-using Plots 
+using GLMakie
+
 
 ## create a blinking fluorophore 
-γ=1e5
-q=[0 50
+γ = 1e5
+q = [0 50
    1e-2 0]
-f=SMLMSim.GenericFluor(γ,q)
+f = SMLMSim.GenericFluor(γ, q)
 
 # pattern is a single molecule 
-pattern=SMLMSim.Point2D()
-ρ=1.0 
-xsize=25.6
-ysize=25.6
-smd_true=SMLMSim.uniform2D(ρ,pattern,xsize,ysize)
-plt=PlotlyJS.plot(scattergl(x=smd_true.x, y=smd_true.y, mode="markers"))
-display(plt)
+pattern = SMLMSim.Point2D()
+ρ = 1.0
+xsize = 25.6
+ysize = 25.6
+smd_true = SMLMSim.uniform2D(ρ, pattern, xsize, ysize)
+
+fig = Figure()
+ax = Axis(fig[1, 1],
+   xlabel="x",
+   ylabel="y",
+   aspect=AxisAspect(1)
+)
+scatter!(smd_true.x, smd_true.y; color=:red)
 
 # generate the kinetic model
-nframes=2000
-framerate=50.0
-smd_model=SMLMSim.kineticmodel(smd_true,f,nframes,framerate;ndatasets=10)
-plt=Plots.histogram(smd_model.photons;xlabel="photons",ylabel="counts")
-display(plt)
+nframes = 2000
+framerate = 50.0
+smd_model = SMLMSim.kineticmodel(smd_true, f, nframes, framerate; ndatasets=10)
+
+ax2 = Axis(fig[1, 2],
+   xlabel="Photons",
+   ylabel="Counts"
+)
+hist!(smd_model.photons)
 
 # make noisy coordinates
-σ_psf=1.3
-smd_noisy=SMLMSim.noise(smd_model,σ_psf)
-plt=PlotlyJS.plot(scattergl(x=smd_noisy.x, y=smd_noisy.y, mode="markers"))
-display(plt)
+σ_psf = 1.3
+smd_noisy = SMLMSim.noise(smd_model, σ_psf)
+scatter!(ax, smd_noisy.x, smd_noisy.y; color=:green)
+
+#show figure
+fig
+
+
 
 
 
