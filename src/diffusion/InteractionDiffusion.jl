42ca19f6bfd7ae24f5d6d7043e13cd7982ffa59d
Keith A. Lidke <kalidke@users.noreply.github.com>
2023-09-12 18:12:32 -0600

    org setup

---
+++ src/diffusion/InteractionDiffusion.jl
@@ -0,0 +1,12 @@
+"""
+This module provides simulation tools for diffusion
+    and interaction between particles.
+"""
+module InteractionDiffusion
+
+using Distributions
+
+include("diffusion.jl")
+include("smoluchowski.jl")
+
+end
